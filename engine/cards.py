a0467626816198bfa2383cde8fe36a58b08562df
mayer-doa-coder <hasan2107004@stud.kuet.ac.bd>
2026-03-08 00:53:47 +0600

    feat: implement card model and deck generation

--- engine/cards.py
+++ engine/cards.py
@@ -5,17 +5,59 @@ Purpose: Manages game cards including suspects, weapons, and locations.
 This module defines card types, manages the deck, and handles card distribution.
 """
 
+import random
+
 
 class Card:
-    # TODO: card with name and category
-    pass
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+
+    def __repr__(self):
+        return f"Card({self.name}, {self.category})"
 
 
-# TODO: suspects list
-# TODO: weapons list
-# TODO: locations list
+suspects = [
+    "Hashem Sir",
+    "Opi Sir",
+    "Tawhid",
+    "Shejan",
+    "Hasina",
+    "Trump",
+]
+
+weapons = [
+    "Knife",
+    "Poison",
+    "Wrench",
+    "Laptop Charger",
+    "Anti Cutter",
+    "Pipe",
+]
+
+locations = [
+    "Library",
+    "Academic Building",
+    "Amar Ekushey Hall",
+    "Cafeteria",
+    "Central Field",
+    "IT Park",
+    "Rokeya Hall",
+    "VC Room",
+    "Pocket Gate",
+]
 
 
 def create_deck():
-    # TODO: build and shuffle full deck
-    pass
+    deck = []
+    for s in suspects:
+        deck.append(Card(s, "suspect"))
+    for w in weapons:
+        deck.append(Card(w, "weapon"))
+    for l in locations:
+        deck.append(Card(l, "location"))
+
+    random.shuffle(deck)
+    return deck
+
+
